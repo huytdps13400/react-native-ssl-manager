b31230bf13def9edb997b612e3b199ab34bdcd55
huymobile <kingonwork@gmail.com>
2024-08-09 17:39:32 +0700

    Update react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -1,12 +1,9 @@
 module.exports = {
   dependency: {
-    'react-native-use-ssl-pinning': {
-      platforms: {
-        android: {
-          packageImportPath:
-            'import com.usesslpinning.UseSslPinningFactory;\nimport com.facebook.react.modules.network.OkHttpClientProvider;',
-          packageInstance: 'new UseSslPinningPackage()',
-        },
+    platforms: {
+      android: {
+        packageImportPath: 'import com.usesslpinning.UseSslPinningFactory',
+        packageInstance: 'new UseSslPinningPackage()',
       },
     },
   },
